45ec1190ce910dd1ad1db3b05966b646ac0aed7f
Teppei Kanayama <kanayama.teppei@gmail.com>
2020-11-07 16:50:03 +0900

    authentication

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
 from typing import Dict, List, Optional, Tuple
 
 from flask import Flask, request
+from flask_jwt import JWT, jwt_required
 from flask_restful import Resource, Api
 
+from security import authenticate, identity
 
 app = Flask(__name__)
 app.secret_key = 'dummy'
 api = Api(app)
 
+jwt = JWT(app, authenticate, identity)
+
 items: List[Dict[str, str]] = []
 
 
 class Item(Resource):
-
+    @jwt_required()
     def get(self, name: str) -> Tuple[Dict[str, Optional[str]], int]:
         item = next(filter(lambda x: x['name'] == name, items), None)
         return dict(item=item), 200 if item else 404
@@ -33,4 +37,5 @@ class ItemList(Resource):
 
 
 api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
 app.run(port=5000, debug=True)
