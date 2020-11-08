6ebda4140da1c453959e24314783920830ae2d25
Teppei Kanayama <kanayama.teppei@gmail.com>
2020-11-08 11:49:00 +0900

    change post method

--- item.py
+++ item.py
@@ -1,5 +1,5 @@
 import sqlite3
-from typing import Dict, List, Optional, Tuple
+from typing import Dict, List, Optional, Tuple, Any
 
 from flask_jwt import jwt_required
 from flask_restful import Resource, reqparse
@@ -9,8 +9,8 @@ class Item(Resource):
     parser = reqparse.RequestParser()
     parser.add_argument('price', type=float, required=True, help='Price of the item')
 
-    @jwt_required()
-    def get(self, name: str) -> Tuple[Dict[str, Optional[str]], int]:
+    @classmethod
+    def find_by_name(cls, name: str) -> Optional[Dict[str, Any]]:
         connection = sqlite3.connect('db/data.db')
         cursor = connection.cursor()
         query = "SELECT * FROM items WHERE name=?"
@@ -18,15 +18,29 @@ class Item(Resource):
         row = result.fetchone()
         connection.close()
         if row:
-            return dict(name=row[0], price=row[1]), 200
+            return dict(name=row[0], price=row[1])
+
+    @jwt_required()
+    def get(self, name: str) -> Tuple[Dict[str, Optional[str]], int]:
+        item = self.find_by_name(name)
+        if item:
+            return item, 200
         return dict(message="Item not found."), 404
 
     def post(self, name: str) -> Tuple[Dict[str, Optional[str]], int]:
-        if next(filter(lambda x: x['name'] == name, items), None) is not None:
+        if self.find_by_name(name):
             return dict(message=f'An item {name} already exists!'), 400
-        data = self.parser.parse_args()
-        item = dict(name=name, price=data['price'])
-        items.append(item)
+        posted_data = self.parser.parse_args()
+        item = dict(name=name, price=posted_data['price'])
+
+        # TODO: functionize
+        connection = sqlite3.connect('db/data.db')
+        cursor = connection.cursor()
+        query = "INSERT INTO items VALUES (?, ?)"
+        cursor.execute(query, (item['name'], item['price']))
+        connection.commit()
+        connection.close()
+
         return item, 201
 
     def delete(self, name: str) -> Tuple[Dict[str, Optional[str]], int]:
