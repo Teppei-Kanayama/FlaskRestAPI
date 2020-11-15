a51926fb0962a8253dc786b3406ffce9bb368c1d
Teppei Kanayama <kanayama.teppei@gmail.com>
2020-11-15 19:29:08 +0900

    not import db in app

--- app.py
+++ app.py
@@ -13,12 +13,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'dummy'
 api = Api(app)
 
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
-
 jwt = JWT(app, authenticate, identity)
 
 api.add_resource(Item, '/item/<string:name>')
@@ -28,6 +22,4 @@ api.add_resource(Store, '/store/<string:name>')
 api.add_resource(StoreList, '/stores')
 
 if __name__ == '__main__':
-    from flask_rest_api.db import db
-    db.init_app(app)
     app.run(port=5000, debug=True)
